dc8a24447d5c4fd1d89d405627ea06f54b471f82
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-02-10 23:40:29 -0500

    Edit email template

--- app/views/layouts/email_template.blade.php
+++ app/views/layouts/email_template.blade.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 
 </head>
-<body style="background-color: #2A2C2B; padding-top: 50px; width:700px; min-height: 400px; margin-left: auto; margin-right: auto;">
+<body>
 
 @include('../includes.email.email_header')
 <div class="container" style="background-color: #fff; width:700px; border-radius: 15px; padding: 35px;">
