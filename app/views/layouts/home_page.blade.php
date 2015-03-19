b65b277c3a2028d01cb8f5997bd4b4a583cb8ea2
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-03-19 13:17:57 -0400

    fix google font link on home layout

--- app/views/layouts/home_page.blade.php
+++ app/views/layouts/home_page.blade.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0">
 
 
-<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Titillium+Web:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Titillium+Web:400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/css/main_styles.css" />
 </head>
