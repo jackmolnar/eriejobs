7164365abe6df7048c60887faf2129a72123275c
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-03-21 10:18:46 -0400

    added OG tags

--- app/views/layouts/home_page.blade.php
+++ app/views/layouts/home_page.blade.php
@@ -6,7 +6,16 @@
 <meta name="description" content="@yield('_description')" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0">
+<link rel="shortcut icon" href="https://eriepajobs.com/favicon.png">
 
+<meta property="og:url"         content="{{ Request::url() }}" />
+<meta property="fb:app_id"      content="281126498752497" />
+<meta property="og:type"        content="website" />
+<meta property="og:title"       content="@yield('_title')" />
+<meta property="og:description" content="@yield('_description')" />
+<meta property="og:image"       content="{{ url('images/og_image.png') }}" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
 
 <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Titillium+Web:400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
