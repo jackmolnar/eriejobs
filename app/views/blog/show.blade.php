01463bfcd032a025211509a7486c559bc4414c55
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-04-03 11:33:02 -0400

    added share this buttons

--- app/views/blog/show.blade.php
+++ app/views/blog/show.blade.php
@@ -22,6 +22,10 @@
         </div>
 
     </div>
+
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54a05ff370e9a58d" async="async"></script>
+
 @stop
 
 @section('_title')
