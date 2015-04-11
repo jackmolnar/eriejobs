93717fdbaaafc156f51bd0df8096345dd372948b
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-04-11 12:14:54 -0400

    adwords conversion tags

--- app/views/includes/ga_tracking/new_account_vp.blade.php
+++ app/views/includes/ga_tracking/new_account_vp.blade.php
@@ -23,6 +23,26 @@
         window._fbq.push(['track', '6022559436105', {'value':'0.00','currency':'USD'}]);
     </script>
     <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6022559436105&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+
+    <!-- Google Code for Seeker Signup Conversion Page -->
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 967835416;
+        var google_conversion_language = "en";
+        var google_conversion_format = "3";
+        var google_conversion_color = "ffffff";
+        var google_conversion_label = "Br4xCLm-jVsQmP6_zQM";
+        var google_remarketing_only = false;
+        /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/967835416/?label=Br4xCLm-jVsQmP6_zQM&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript>
+
 @endif
 
 @if(Session::has('new_recruiter_account'))
@@ -50,4 +70,24 @@
         window._fbq.push(['track', '6022634491705', {'value':'0.00','currency':'USD'}]);
     </script>
     <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6022634491705&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+
+    <!-- Google Code for Recruiter Signup Conversion Page -->
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 967835416;
+        var google_conversion_language = "en";
+        var google_conversion_format = "3";
+        var google_conversion_color = "ffffff";
+        var google_conversion_label = "mEFbCMTs91oQmP6_zQM";
+        var google_remarketing_only = false;
+        /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/967835416/?label=mEFbCMTs91oQmP6_zQM&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript>
+
 @endif
