6c329bcb05d8493170e3f6fdd63ff4577671d1a2
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-04-13 22:29:50 -0400

    new share buttons for blog

--- app/views/blog/show.blade.php
+++ app/views/blog/show.blade.php
@@ -7,6 +7,11 @@
         <div class="col-md-10">
             <h1>{{ $post->title }}</h1>
             <p class="date"><i class="fa fa-calendar"></i> {{ $post->created_at->format('F jS Y') }}</p>
+            <span class='st_facebook_vcount' displayText='Facebook'></span>
+            <span class='st_twitter_vcount' displayText='Tweet'></span>
+            <span class='st_linkedin_vcount' displayText='LinkedIn'></span>
+            <span class='st_googleplus_vcount' displayText='Google +'></span>
+            <span class='st_email_vcount' displayText='Email'></span>
         </div>
         <div class="col-md-2">
             {{ link_to_action('BlogController@index', 'Back To Posts', null, ['class' => 'btn btn-default btn-xs posts_back_button']) }}
@@ -30,9 +35,9 @@
     </div>
 
 
-    <!-- Go to www.addthis.com/dashboard to customize your tools -->
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54a05ff370e9a58d" async="async"></script>
-
+    <script type="text/javascript">var switchTo5x=true;</script>
+    <script type="text/javascript" src="https://ws.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript">stLight.options({publisher: "dc971248-c84a-475a-9b16-e8927be4f6bc", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 @stop
 
 @section('_title')
