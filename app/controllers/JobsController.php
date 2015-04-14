cbaaf439290df73f11f43243115e6160490e4d92
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-04-13 22:56:33 -0400

    rollback

--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -139,10 +139,10 @@ class JobsController extends \BaseController {
 	{
         $job = $this->jobRepo->getJobById($id);
 
-        if(!isset($job) || !$job->confidential)
-        {
-            return View::make('jobs.not_found');
-        }
+//        if(!isset($job) || !$job->confidential)
+//        {
+//            return View::make('jobs.not_found');
+//        }
 
         $categories = $this->categoryRepo->getAllCategories();
         return View::make('jobs.show', ['job' => $job, 'categories' => $categories]);
