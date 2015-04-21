5169c4e4db4429a49ddf79362704b6f01cb4cbe7
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-04-21 08:49:19 -0400

    set search limit to 100

--- app/EriePaJobs/Jobs/JobsRepository.php
+++ app/EriePaJobs/Jobs/JobsRepository.php
@@ -112,7 +112,7 @@ class JobsRepository {
      * @param string $term
      * @return \Elasticquent\ElasticquentResultCollection
      */
-    public function searchForJob($term = '', $limit = null, $offset = null)
+    public function searchForJob($term = '', $limit = 100, $offset = null)
     {
         if($term == ''){
             $result = $this->allActiveJobs();
