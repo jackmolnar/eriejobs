ca9e2ea2000c09da0157af66eefd7bedb6b9fd2f
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-05-04 20:33:00 -0400

    skyscraper on search page

--- app/views/search/index.blade.php
+++ app/views/search/index.blade.php
@@ -33,6 +33,19 @@
     @include('includes.jobs.pagination')
 
     </div>
+
+    <div class="col-md-3">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Test Ads -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:300px;height:600px"
+             data-ad-client="ca-pub-5103028415668122"
+             data-ad-slot="7829958692"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+
 </div>
 
 @stop
