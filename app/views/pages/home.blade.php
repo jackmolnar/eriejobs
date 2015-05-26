878898011becfe8f93c26048180ec7ad07045da3
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-05-25 21:05:54 -0400

    fix home page

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -5,7 +5,7 @@
 <div class="main_back">
     <div class="container home_page">
         <div class="main_home">
-             <h1>Erie and Northwestern Pennsylvania's Exclusive Employment Website</h1>
+             <h1>Erie's and Northwestern Pennsylvania's Exclusive Employment Website</h1>
              <div class="home_buttons">
                  {{ link_to_action('AuthController@getSeekerSignup', 'Signup', null, ['class' => 'btn btn-primary btn-lg']) }}
                  <span>OR</span>
