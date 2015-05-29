f1160377ad260cac30c367408d087257d7d88af9
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-05-28 20:50:36 -0400

    strip tags indeed feed

--- app/views/feeds/indeed.blade.php
+++ app/views/feeds/indeed.blade.php
@@ -12,7 +12,7 @@
             <city><![CDATA[ {{ $job->city }} ]]></city>
             <state><![CDATA[ {{ $job->state->title }} ]]></state>
             <country><![CDATA[US]]></country>
-            <description><![CDATA[ {{ $job->description }} ]]></description>
+            <description><![CDATA[ {{ strip_tags($job->description) }} ]]></description>
             <salary><![CDATA[ {{ $job->salary }} ]]></salary>
             <jobtype><![CDATA[ {{ $job->type->type }} ]]></jobtype>
             <category><![CDATA[ {{ $job->categories->first()->category }}]]></category>
