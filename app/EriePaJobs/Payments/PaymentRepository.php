5f7e4bbb227635c7d4790f808bd8400f191b9328
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-07-14 14:06:18 -0400

    add slashes

--- app/EriePaJobs/Payments/PaymentRepository.php
+++ app/EriePaJobs/Payments/PaymentRepository.php
@@ -26,13 +26,13 @@ class PaymentRepository {
         Stripe::setApiKey(\Config::get('services.stripe.secret'));
 
         // Get the credit card details submitted by the form
-        $token = Input::get('stripeToken');
+        $token = \Input::get('stripeToken');
 
         // Create the charge on Stripe's servers - this will charge the user's card
         try
         {
             $charge = Stripe_Charge::create(array(
-                    "amount" => Input::get('cost'), // amount in cents, again
+                    "amount" => \Input::get('cost'), // amount in cents, again
                     "currency" => "usd",
                     "card" => $token,
                     "description" => "payinguser@example.com")
