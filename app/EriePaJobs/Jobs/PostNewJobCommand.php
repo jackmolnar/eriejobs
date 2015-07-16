f4cb82dd55ebf4e71200ecf656c985ab7102fce5
Jonathan Molnar <jackmolnar1982@gmail.com>
2015-07-16 14:51:04 -0400

    make admin post free

--- app/EriePaJobs/Jobs/PostNewJobCommand.php
+++ app/EriePaJobs/Jobs/PostNewJobCommand.php
@@ -95,7 +95,7 @@ class PostNewJobCommand extends BaseCommand{
         $listingsLeft = $this->userRepo->remainingSubscribedJobs();
 
         // if not free, bill
-        if(!Config::get('billing.free') && !$listingsLeft) {
+        if(!Config::get('billing.free') && !$listingsLeft && $this->user->role->title != 'Administrator') {
             $result = $this->paymentRepo->bill($this->input);
         }
 
